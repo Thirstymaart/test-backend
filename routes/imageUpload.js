59e6583ef58cd69dd05f3dda9edfa1a61e250ed0
Thirstymaart <152849871+Thirstymaart@users.noreply.github.com>
2023-12-25 20:03:00 +0530

    Update imageUpload.js

--- routes/imageUpload.js
+++ routes/imageUpload.js
@@ -34,7 +34,7 @@ router.post('/upload', verifyToken, upload.single('image'), async (req, res) => 
       await fs.writeFile(imagePath, req.file.buffer);
 
       // Construct the full URL including the server domain and path
-      const fullUrl = `http://localhost:3001/uploads/${vendorId}/${imageName}`;
+      const fullUrl = `https://thirstymaart.com/api/uploads/${vendorId}/${imageName}`;
   
       res.json({ imageUrl: fullUrl });
     } catch (error) {
